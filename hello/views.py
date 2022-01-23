f1d1bce84f3e841a849f306ae653f0d773067baa
Roburlion <94622793+Roburlion@users.noreply.github.com>
2022-01-22 21:42:46 -0600

    8008135

--- hello/views.py
+++ hello/views.py
@@ -13,10 +13,10 @@ def index(request):
     # print(r.text)
     # return HttpResponse('<pre>' + r.text + '</pre>')
 
-    times = int(os.environ.get('TIMES', 3))
-    return HttpResponse('Hello! ' * times)
+    # times = int(os.environ.get('TIMES', 3))
+    # return HttpResponse('Hello! ' * times)
     
-    # return render(request, "index.html")
+    return render(request, "index.html")
 
 
 def db(request):
