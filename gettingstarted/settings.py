90021bd32b01c74dfeb4d673f093c9e8fc359f8b
Roburlion <94622793+Roburlion@users.noreply.github.com>
2022-01-25 16:23:33 -0600

    err db issue attempt BigAutoField

--- gettingstarted/settings.py
+++ gettingstarted/settings.py
@@ -117,3 +117,8 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = "/static/"
 
 django_heroku.settings(locals())
+
+# Default primary key field type
+# https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
